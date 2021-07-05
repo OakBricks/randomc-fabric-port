d498e6cf57fd816a8181f1caab49fe5dcf912d92
OakBricks <oakbricks@gmail.com>
2021-07-04 23:31:26 -0500

    disc part one

---
+++ src/main/java/curseforge/airplane39/randomc/item/TwentyTwentyOneDiscItem.java
@@ -0,0 +1,10 @@
+package curseforge.airplane39.randomc.item;
+
+import net.minecraft.item.MusicDiscItem;
+import net.minecraft.sound.SoundEvent;
+
+public class TwentyTwentyOneDiscItem extends MusicDiscItem {
+    protected TwentyTwentyOneDiscItem(int comparatorOutput, SoundEvent sound, Settings settings) {
+        super(comparatorOutput, sound, settings);
+    }
+}
